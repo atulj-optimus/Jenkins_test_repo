b99385e069f76f7538fd36dbbc5eca2d704e8fa4
atulj-optimus <atul.jain@optimusinfo.com>
2012-12-20 15:18:25 +0530

    Added Jenkins web app

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary1;
+
+namespace WebApplication1
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        Class1 c = new Class1();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = c.show();
+        }
+    }
+}
